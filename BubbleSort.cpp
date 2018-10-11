d1e94e49925ba18040895982ae36d1536536eff7
Kayann Soares <kayan.soares1@gmail.com>
2018-10-11 08:02:28 -0300

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <conio.h>
+
 using namespace std;
 
 
@@ -8,27 +9,27 @@ int main() {
     //clrscr();
 
     // Carregando os numeros no vetor
-    for(int i=0;i<=4;i++){
-        cout << "Digite o" << i+1 << " numero: ";
+    for (int i = 0; i <= 4; i++) {
+        cout << "Digite o " << i + 1 << " numero: ";
         cin >> x[i];
     }
+
     // Ordeando de forma Crescente
     // Laço com a quantidade de elementos do vetor
-
-    for(int n=0;n<=5;n++){
+    for (int n = 0; n <= 5; n++) {
         // Laço que percorre da primeira a penultima posição do vetor
-        for(int i=0;i<=3;i++){
-            if(x[i] > x[i+1]){
+        for (int i = 0; i <= 3; i++) {
+            if (x[i] > x[i + 1]) {
                 aux = x[i];
-                x[i] = x[i+1];
-                x[i+1] = aux;
+                x[i] = x[i + 1];
+                x[i + 1] = aux;
             }
         }
 
     }
     // Mostrando o vetor ordenado
-    for(int i=0;i<=4;i++){
-        cout << i+1 << " numero: " << x[i] << "\n" ;
+    for (int i = 0; i <= 4; i++) {
+        cout << i + 1 << " numero: " << x[i] << "\n";
     }
     getch();
     return 0;
