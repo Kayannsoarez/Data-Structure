3373cf2a7f9e910eef83ecf35b90dd70da710d49
Kayann Soares <kayan.soares1@gmail.com>
2018-11-12 02:02:37 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+// Created by kayann on 10/11/2018.
+//
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main() {
+    int x[5],j, i, aux;
+    //clrscr();
+
+    // Carregando os numeros no vetor
+    for (int i = 0; i <= 4; i++) {
+        cout << "Digite o " << i + 1 << " numero: ";
+        cin >> x[i];
+    }
+
+    // Ordeando de forma Crescente
+    // Laço com a quantidade de elementos do vetor - 1
+    for (int j = 1; j <= 4; j++) {
+        // Laço que percorre da ultima posição a posição j do vetor
+        for (int i = 4; i >= j; i--) {
+            if (x[i] < x[i - 1]) {
+                aux = x[i];
+                x[i] = x[i - 1];
+                x[i - 1] = aux;
+            }
+        }
+
+    }
+    // Mostrando o vetor ordenado
+    for (int i = 0; i <= 4; i++) {
+        cout << i + 1 << " numero: " << x[i] << "\n";
+    }
+    getch();
+    return 0;
+}
