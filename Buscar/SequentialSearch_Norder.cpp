da109b2a8257a51d14793566e72e864ae6236908
Kayann Soares <kayan.soares1@gmail.com>
2021-06-08 19:07:27 -0300

    Upload

---
+++ Buscar/SequentialSearch_Norder.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int x[10],i, n, achou;
+
+  // Carregando os numeros no vetor (Não Ordenado)
+  for(int i=0;i<=9;i++){
+    cout << "Digite o " << i+1 << "° numero: ";
+    cin >> x[i];
+  }
+
+  // Numero a ser buscado no vetor
+  cout << "\nDigite o numero a ser buscado: ";
+  cin >> n;
+
+  // Buscando o numero no vetor
+  i = 0;
+  achou = 0;
+  while(i <= 9 && achou == 0){
+    if(x[i]==n){
+      achou = 1;
+    }else{
+      i++;
+    }
+  }
+  if(achou == 0){
+    cout << "Numero não encontrado";
+  }
+  else{
+    cout << "Numero encontrado na posição " << i+1;
+  }
+  return 0;
+
+}
